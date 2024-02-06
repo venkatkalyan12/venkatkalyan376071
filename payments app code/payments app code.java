4daf63fb12f431f4d85ee77ffec17c763e009faf
venkatkalyan <venkatkalyanj99@gmail.com>
2024-02-06 07:29:12 +0530

    kk

---
+++ payments app code/payments app code.java
@@ -0,0 +1,14 @@
+public class payments app code {
+	public static void main (String[] args) {
+	Scanner in =new Scanner(System.in);
+	System.out.println("ENTER YOUR NAME:");
+	String name=in.nextLine();
+	System.out.println("age:");
+	String age=in.nextLine();
+	System.out.println("Gender:");
+	String Gender=in.nextLine();
+	System.out.println("MALE,FEMALE:");
+	String gen=in.next();
+	in.close();
+	}
+	}
